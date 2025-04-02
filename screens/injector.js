758bbd900410260b888e74df9fdcf15b52333da3
Devmike01 <michaelgbenga91@gmail.com>
2025-04-02 17:50:38 +0100

    Added screens

---
+++ screens/injector.js
@@ -0,0 +1,20 @@
+// Add every api that needs to be provided here
+const paths = [
+    require('./index'),
+    require('./onboard'),
+    require('./users'),
+];
+
+// Inject all api screens
+function inject(app){
+    paths.forEach(path => {
+        if (typeof path === 'function'){
+            app.use('/', path);
+        }else{
+            console.warn('Invalid route', path);
+        }
+    });
+}
+
+
+module.exports = {inject};
